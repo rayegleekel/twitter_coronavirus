4859698c5a3570c750975a5f83a773a6bcac9c19
Raye Gleekel <raye.gleekel@pomona.edu>
2020-04-27 18:42:05 -0700

    twitter corona analysis!

--- src/map.py
+++ src/map.py
@@ -37,6 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country = defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
@@ -64,6 +65,15 @@ with zipfile.ZipFile(args.input_path) as archive:
                         counter_lang[hashtag][lang] += 1
                     counter_lang['_all'][lang] += 1
 
+                for hashtag in hashtags:
+                    try:
+                        country = tweet['place']['country_code']
+                        if hashtag in text:
+                            counter_country[hashtag][country] += 1
+                        counter_country['_all'][country] += 1
+                    except TypeError:
+                        pass
+
 # open the outputfile
 try:
     os.makedirs(args.output_folder)
@@ -72,7 +82,13 @@ except FileExistsError:
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
+output_path_country = output_path_base+'.country'
+
 print('saving',output_path_lang)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
+
+print('saving', output_path_country)
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
 
